24d40d4d846103275b6ae784fd54f4a572037dff
Salman <Salmanabdulghani.cis@gmail.com>
2022-05-20 14:58:47 +0500

    22- Solution- Like Component

---
+++ src/components/common/Like.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Like = (props) => {
+    let classes = "fa fa-heart";
+    if (!props.isLiked) classes += "-o";
+
+    return (<i
+        onClick={props.onClick}
+        className={classes}
+        aria-hidden="true"></i>);
+}
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { getMovies } from '../services/fakeMovieService';
+import Like from './common/Like';
 
 class Movies extends Component {
 
@@ -12,6 +13,16 @@ class Movies extends Component {
         this.setState({ movies });
     }
 
+    handleLike = (movie) => {
+        const movies = [...this.state.movies];
+        const i = movies.indexOf(movie);
+
+        movies[i] = { ...movies[i] };
+        movies[i].liked = !movies[i].liked;
+
+        this.setState({ movies });
+    }
+
     render() {
         const { length: count } = this.state.movies;
 
@@ -21,35 +32,44 @@ class Movies extends Component {
 
         return (
             <React.Fragment>
-                <div class="alert alert-info">
+                <div className="alert alert-info">
                     Showing <strong>{count}</strong> movies in the database.
                 </div>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.movies.map(movie =>
-                                <tr key={movie._id}>
-                                    <td>{movie.title}</td>
-                                    <td>{movie.genre.name}</td>
-                                    <td>{movie.numberInStock}</td>
-                                    <td>{movie.dailyRentalRate}</td>
-                                    <td>
-                                        <button onClick={() => this.handleDeleteMovie(movie)} className='btn btn-danger'>Delete</button>
-                                    </td>
-                                </tr>
-                            )}
+                <div className="table-responsive">
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Genre</th>
+                                <th>Stock</th>
+                                <th>Rate</th>
+                                <th className='text-center'>Favorite</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.movies.map(movie =>
+                                    <tr key={movie._id}>
+                                        <td>{movie.title}</td>
+                                        <td>{movie.genre.name}</td>
+                                        <td>{movie.numberInStock}</td>
+                                        <td>{movie.dailyRentalRate}</td>
+                                        <td className='text-center'>
+                                            <Like
+                                                isLiked={movie.liked}
+                                                onClick={() => this.handleLike(movie)} />
+                                        </td>
+                                        <td>
+                                            <button onClick={() => this.handleDeleteMovie(movie)} className='btn btn-danger'>Delete</button>
+                                        </td>
+                                    </tr>
+                                )}
 
-                    </tbody>
-                </table>
+                        </tbody>
+                    </table>
+                </div>
+
             </React.Fragment>
         );
     }
