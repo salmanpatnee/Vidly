68fbb75e2ca73a2e61fb407cbe3423a0aabc3646
Salman Patnee <salmanpatni92@gmail.com>
2022-05-27 21:55:01 +0500

    Add Back to Movies Button

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,25 +1,19 @@
-import React, { Component } from 'react';
-import { withRoter } from 'react-router-dom';
+import React from 'react';
+import { useParams, useNavigate  } from 'react-router-dom';
 
-class MovieDetails extends Component {
-    state = {
-        id: 5
-    }
-
-
-
-    render() {
-        console.log(this.props);
-        return (
-            <div>
-                <div className="jumbotron">
-                    <h1 className="display-4">Movie Details - {this.id}</h1>
-                </div>
-
-                <button onClick={this.saveMovie} className="btn btn-primary">Save</button>
+const MovieDetails = props => {
+    
+    const { id } = useParams();
+    const navigate = useNavigate();
+    
+    return ( 
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4">Movie Details - {id}</h1>
             </div>
-        );
-    }
+            <button onClick={() => navigate(-1)} className="btn btn-primary">Save</button>
+        </div>
+     );
 }
-
+ 
 export default MovieDetails;
